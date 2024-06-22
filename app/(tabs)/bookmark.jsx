dedb1d1b1aa0469628cf65ae2f0b04e1bb3474df
sagar201993 <sagarvirk1@gmail.com>
2024-06-22 08:47:13 +0200

    chnage

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,7 @@
-import { View, Text } from "react-native";
 import React from "react";
 
 const bookmark = () => {
-  return (
-    <View>
-      <Text>bookmark</Text>
-    </View>
-  );
+  return <div>bookmark</div>;
 };
 
 export default bookmark;
